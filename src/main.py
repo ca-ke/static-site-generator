5c3352bd3d955390ea3c909a76faec18bd712c77
ca-ke <minhare.caique@gmail.com>
2025-02-27 20:56:05 -0300

    fix: Adjusting path

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ import sys
 def main():
     basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
 
-    delete_files_from("doc")
-    copy_from_source_to_destination("static", "doc")
-    generate_page_recursive(basepath, "content", "template.html", "doc")
+    delete_files_from("docs")
+    copy_from_source_to_destination("static", "docs")
+    generate_page_recursive(basepath, "content", "template.html", "docs")
 
 
 if __name__ == "__main__":
